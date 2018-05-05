06599535b84a1ec064bb7951c12737f65b6fc696
obiwanzenobi <wojciech.warwas@iteo.com>
2018-05-05 23:14:22 +0200

    Create HelloGeneratedComponent.java

---
+++ sample/java/HelloGeneratedComponent.java
@@ -0,0 +1,45 @@
+public final class DaggerAppComponent implements AppComponent {
+  private AppModule appModule;
+
+  private DaggerAppComponent(Builder builder) {
+    initialize(builder);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @SuppressWarnings("unchecked")
+  private void initialize(final Builder builder) {
+    this.appModule = builder.appModule;
+  }
+
+  @Override
+  public void inject(MainActivity activity) {
+    injectMainActivity(activity);
+  }
+
+  private MainActivity injectMainActivity(MainActivity instance) {
+    MainActivity_MembersInjector.injectInitialText(
+        instance, AppModule_ProvideHelloFactory.proxyProvideHello(appModule));
+    return instance;
+  }
+
+  public static final class Builder {
+    private AppModule appModule;
+
+    private Builder() {}
+
+    public AppComponent build() {
+      if (appModule == null) {
+        throw new IllegalStateException(AppModule.class.getCanonicalName() + " must be set");
+      }
+      return new DaggerAppComponent(this);
+    }
+
+    public Builder appModule(AppModule appModule) {
+      this.appModule = Preconditions.checkNotNull(appModule);
+      return this;
+    }
+  }
+}
