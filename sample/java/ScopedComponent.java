d0b984d14808c47667f508526de796da5092e280
obiwanzenobi <wojtek.warwas@gmail.com>
2018-05-06 12:16:31 +0200

    Create ScopedComponent.java

---
+++ sample/java/ScopedComponent.java
@@ -0,0 +1,49 @@
+@Scope
+@Retention(RUNTIME)
+public @interface AppScope {
+}
+
+@Module
+public class AppModule {
+
+    private Context context;
+
+    public AppModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    @AppScope
+    public String provideHello() {
+        return context.getString(R.string.hello);
+    }
+}
+
+
+public final class DaggerAppComponent implements AppComponent {
+  private Provider<String> provideHelloProvider;
+
+  private DaggerAppComponent(Builder builder) {
+    initialize(builder);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @SuppressWarnings("unchecked")
+  private void initialize(final Builder builder) {
+    this.provideHelloProvider =
+        DoubleCheck.provider(AppModule_ProvideHelloFactory.create(builder.appModule));
+  }
+
+  @Override
+  public void inject(MainActivity activity) {
+    injectMainActivity(activity);
+  }
+
+  private MainActivity injectMainActivity(MainActivity instance) {
+    MainActivity_MembersInjector.injectInitialText(instance, provideHelloProvider.get());
+    return instance;
+  }
+}
